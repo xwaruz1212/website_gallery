b84083b79f7764182f7e87c2f919126a0e3f1121
Kacper <kaczur190@gmail.com>
2022-07-28 02:37:55 +0200

    Positioning image and arrows

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './index.scss';
+import Paintings from './paintings';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
-const Gallery = () => {
+
+class Gallery extends React.Component {
+  state = {  }
+  render() { 
     return (
-      <div className='i-center'>
-        <br />
-        <h1>
-            My Paintings
-        </h1>
+      <> 
+      <div className='image-cont'>
+      <img src={Paintings} alt="" className='img'/>
       </div>
+      <section>
+        <FaArrowAltCircleLeft className='left-arrow'/>
+        <FaArrowAltCircleRight className='right-arrow'/>
+      </section>
+      </>
      );
+  }
 }
  
 export default Gallery;
---
+++ src/components/Gallery/paintings.js
@@ -0,0 +1,4 @@
+import MakisPhoto from '../../assets/images/Makis-Photo.png'
+const Paintings = [MakisPhoto]
+ 
+export default Paintings;
