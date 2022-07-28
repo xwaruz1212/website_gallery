c48799c22a921ae2d5e0a555318663e86e59e505
Kacper <kaczur190@gmail.com>
2022-07-28 02:37:07 +0200

    Added new styles for navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faCamera, faBookReader } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faPalette, faBookReader } from '@fortawesome/free-solid-svg-icons'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
@@ -9,13 +9,13 @@ const Navbar = () => {
       <div className="nav-bar">
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faHome} color="#ffff" />
         </NavLink>
         <NavLink activeclassname="active" className="Books-link" to="/Books">
-          <FontAwesomeIcon icon={faBookReader} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faBookReader} color="#ffff" />
         </NavLink>
         <NavLink activeclassname="active" className="Gallery-link" to="/Gallery">
-          <FontAwesomeIcon icon={faCamera} color="#4d4d4e" />
+          <FontAwesomeIcon icon={faPalette} color="#ffff" />
         </NavLink>
         
       </nav>
