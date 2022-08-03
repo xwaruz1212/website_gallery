c148d462964e1e6a506a6ea7e9617a88d76d502d
Kacper <kaczur190@gmail.com>
2022-08-03 11:33:10 +0200

    gallery assets added

--- src/components/Gallery/paintings.js
+++ src/components/Gallery/paintings.js
@@ -6,6 +6,33 @@ import img5 from '../../assets/paintings/img5.png'
 import img6 from '../../assets/paintings/img6.png'
 import img7 from '../../assets/paintings/img7.png'
 import img8 from '../../assets/paintings/img8.png'
+import img9 from '../../assets/paintings/img9.png'
+import img10 from '../../assets/paintings/img10.png'
+import img11 from '../../assets/paintings/img11.png'
+import img12 from '../../assets/paintings/img12.png'
+import img13 from '../../assets/paintings/img13.png'
+import img14 from '../../assets/paintings/img14.png'
+import img15 from '../../assets/paintings/img15.png'
+import img16 from '../../assets/paintings/img16.png'
+import img17 from '../../assets/paintings/img17.png'
+import img18 from '../../assets/paintings/img18.png'
+import img19 from '../../assets/paintings/img19.png'
+import img20 from '../../assets/paintings/img20.png'
+import img21 from '../../assets/paintings/img21.png'
+import img22 from '../../assets/paintings/img22.png'
+import img23 from '../../assets/paintings/img23.png'
+import img24 from '../../assets/paintings/img24.png'
+import img25 from '../../assets/paintings/img25.png'
+import img26 from '../../assets/paintings/img26.png'
+import img27 from '../../assets/paintings/img27.png'
+import img28 from '../../assets/paintings/img28.png'
+import img29 from '../../assets/paintings/img29.png'
+import img30 from '../../assets/paintings/img30.png'
+import img31 from '../../assets/paintings/img31.png'
+import img32 from '../../assets/paintings/img32.png'
+import img33 from '../../assets/paintings/img33.png'
+import img34 from '../../assets/paintings/img34.png'
+import img35 from '../../assets/paintings/img35.png'
 
 const Paintings = [
     img1,
@@ -15,8 +42,34 @@ const Paintings = [
     img5,
     img6,
     img7,
-    img8
-
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+    img17,
+    img18,
+    img19,
+    img20,
+    img21,
+    img22,
+    img23,
+    img24,
+    img25,
+    img26,
+    img27,
+    img28,
+    img29,
+    img30,
+    img31,
+    img32,
+    img33,
+    img34,
+    img35
 ]
  
 export default Paintings;
