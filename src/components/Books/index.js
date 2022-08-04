c2975330487d97445477bf22834d9f07081d9429
Kacper <kaczur190@gmail.com>
2022-08-04 14:58:05 +0200

    Pdf books with design added

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import MagicalWorld from '../../assets/images/MagicalWorld.png'
+import Sofianos from '../../assets/images/Sofianos.png'
+import './index.scss';
 
 const Books = () => {
     return ( 
-        <h1>
-            <br />
-            My books
-        </h1>
+        <>
+            <section className='title'>
+                MY BOOKS
+            </section>
+            <div>
+                <a href='https://drive.google.com/file/d/1fOfXeyHRbk6ua3a783tVKmiL7JIDn-5i/view?usp=sharing'>
+                <img src={MagicalWorld} alt="" className='img-right' />
+                </a>
+                <a href='https://drive.google.com/file/d/11TGRcWaG8tAMOS0ZiDw8JrtLaPEHaq3o/view?usp=sharing'>
+                <img src={Sofianos} alt="" className='img-left'/>
+                </a>
+            </div>
+         </>
+
      );
 }
  
