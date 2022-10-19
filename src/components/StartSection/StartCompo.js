bf308877bd1b54c6906d28afbb61eb30be5b3f60
Kacper <kaczur190@gmail.com>
2022-10-19 22:53:45 +0100

    StartComponents added

---
+++ src/components/StartSection/StartCompo.js
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+export const StartContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: white;
+`;
+
+export const StartH2 = styled.h2`
+    color: black;
+    font-size: 4vh;
+`;
+
+export const ImageContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Image = styled.image`
+    max-width: 8vw;
+    max-height: 12vh;
+`;
+
+export const StartP = styled.p`
+    color: black;
+    font-size: 2vh;
+`;
