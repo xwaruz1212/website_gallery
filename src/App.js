18f793f62549b66b214c2d088774960eeed8ae41
Kacper <kaczur190@gmail.com>
2022-10-20 01:04:22 +0100

    app structure updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import Books from './components/Books'
-import Gallery from './components/Gallery'
-import Layout from './components/Layout'
-import './index.scss'
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="Books" element={<Books />} />
-          <Route path="Gallery" element={<Gallery />} /> 
-        </Route>
-      </Routes>
-    </>
+    <Router>
+      <Home />
+    </Router>
   );
-}
+};
 
 export default App;
